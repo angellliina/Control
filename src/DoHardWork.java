1719596e576d486caabcd3c229fe1ebb1ac85366
angellliina <91912338+angellliina@users.noreply.github.com>
2022-11-21 21:40:04 +0200

    Add files via upload

---
+++ src/DoHardWork.java
@@ -0,0 +1,7 @@
+public class DoHardWork {
+        LoggerSingleton logger = LoggerSingleton.GetInstance();
+        HardProcessor processor = new HardProcessor(1);
+        logger.Log("Hard work started...");
+        processor.ProcessTo(5);
+        logger.Log("Hard work finished...");
+}
---
+++ src/LoggerSingleton.java
@@ -0,0 +1,21 @@
+import java.io.Console;
+
+class LoggerSingleton
+{
+    private LoggerSingleton() { }
+    private int _logCount = 0;
+    private static LoggerSingleton _loggerSingletonInstance = null;
+    public static LoggerSingleton GetInstance()
+    {
+        if (_loggerSingletonInstance == null)
+        {
+            _loggerSingletonInstance = new LoggerSingleton();
+        }
+        return _loggerSingletonInstance;
+    }
+    public void Log(String message)
+    {
+        Console.WriteLine(_logCount + ": " + message);
+        _logCount++;
+    }
+}
